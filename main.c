46e1381765e6f5f9edcc9f3bdadfe384ea850643
AlessandraMamani <mmamaniadc@unsa.edu.pe>
2026-06-11 14:52:46 -0500

    a

--- main.c
+++ main.c
@@ -4,15 +4,13 @@
 
 int comparePirates(const void *x, const void *y);
 
-int main(void)
-{
+int main(void){
     int n;
     scanf("%d", &n);
 
     Piratas pirates[1000];
 
-    for(int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++){
         scanf("%s %lld %d %s",
               pirates[i].name,
               &pirates[i].bounty,
@@ -20,15 +18,9 @@ int main(void)
               pirates[i].crew);
     }
 
-    insertionSort(
-        pirates,
-        n,
-        sizeof(Piratas),
-        comparePirates
-    );
+    insertionSort(pirates, n, sizeof(Piratas), comparePirates);
 
-    for(int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++){
         printf("%s %lld %d %s\n",
                pirates[i].name,
                pirates[i].bounty,
